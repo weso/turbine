4f3233d2de0457a441fb06558a0aeb3907545cc6
Guillermo Facundo Colunga <colunga91@gmail.com>
2019-04-14 22:59:41 +0200

    [code] add shaclex validator adapter

---
+++ src/main/java/org/weso/turbine/validators/ShaclexValidatorAdapter.java
@@ -0,0 +1,19 @@
+package org.weso.turbine.validators;
+
+
+import org.weso.turbine.streams.RDFStream;
+
+public class ShaclexValidatorAdapter implements RDFValidator {
+
+    @Override
+    public RDFValidatorResult validate(RDFStream rdfStream) {
+        String graphToValidate = rdfStream.getGraph();
+        String scheema = rdfStream.getSchema();
+        String shapeMap = rdfStream.getShapeMap();
+
+        // Validation must be done here...
+        // Validator.validate()
+
+        return new DefaultRDFValidatorResult(rdfStream, "", false);
+    }
+}
