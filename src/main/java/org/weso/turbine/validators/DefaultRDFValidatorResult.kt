e18802ea08dca4f04d7bcb3fc88fc7b6d35e5273
Guillermo Facundo Colunga <colunga91@gmail.com>
2019-04-14 23:00:05 +0200

    [code] add default rdf validator result implementation

---
+++ src/main/java/org/weso/turbine/validators/DefaultRDFValidatorResult.kt
@@ -0,0 +1,24 @@
+package org.weso.turbine.validators
+
+import org.weso.turbine.streams.RDFStream
+
+/**
+ * Default implementation of an rdf validated stream.
+ *
+ * @author Guillermo Facundo Colunga
+ * @version 20180414
+ */
+data class DefaultRDFValidatorResult(private val validatedRDFStream: RDFStream, private val resultDescription: String, private val isCorrect: Boolean) : RDFValidatorResult {
+
+    override fun getValidatedRDFStream(): RDFStream {
+        return this.validatedRDFStream
+    }
+
+    override fun getResultDescription(): String {
+        return this.resultDescription
+    }
+
+    override fun isCorrect(): Boolean {
+        return this.isCorrect
+    }
+}
