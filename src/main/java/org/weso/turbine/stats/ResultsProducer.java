a20c48e3761d6724384436b47f89da709179f667
Guillermo Facundo Colunga <colunga91@gmail.com>
2019-04-14 22:53:28 +0200

    [code] add results producer interface
    Adds the results producer interface to produce results after its publication.

---
+++ src/main/java/org/weso/turbine/stats/ResultsProducer.java
@@ -0,0 +1,42 @@
+package org.weso.turbine.stats;
+
+import org.weso.turbine.validators.RDFValidatorResult;
+
+import java.util.Collection;
+
+/**
+ * A results producer is the object that will be published as a JSON or as whatever the user wants to.
+ *
+ * @author Guillermo Facundo Colunga
+ * @version 20180414
+ */
+public interface ResultsProducer {
+
+    /**
+     * Gets the number of correct validations that had occur until this method is called.
+     *
+     * @return the number of correct validations.
+     */
+    String getNumberOfCorrectValidations();
+
+    /**
+     * Gets the number of incorrect validations that had occur until this method is called.
+     *
+     * @return the number of incorrect validations.
+     */
+    String getNumberOfIncorrectValidations();
+
+    /**
+     * Gets the percentage of correct validations that had occur until this method is called.
+     *
+     * @return the percentage of correct validations.
+     */
+    String getPercentageOfCorrectValidations();
+
+    /**
+     * Gets the incorrect validations objects that had occur until this method is called.
+     *
+     * @return the incorrect validations objects.
+     */
+    Collection<RDFValidatorResult> getIncorrectValidationResults();
+}
